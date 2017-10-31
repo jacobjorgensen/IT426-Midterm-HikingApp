466e859a423d2c425a6f6838e2e6545cd3b2b1aa
macklarson <mlarson28@mail.greenriver.edu>
2017-10-31 13:58:27 -0700

    Started adding buttons and styling home page

--- HikingApp/src/main/java/ui/HomePage.java
+++ HikingApp/src/main/java/ui/HomePage.java
@@ -1,29 +1,73 @@
 package ui;
 
 import controller.Dashboard;
-import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+
 
 public class HomePage
 {
+    private static Text title = new Text("Hello, Jose!");
+    private static String[] buttons = {"Hikes", "Tasks", "Messages"};
+    private static final int BUTTON_HEIGHT = 75;
+    private static final int BUTTON_WIDTH = 200;
+
     public static Scene getScene()
     {
-        Text text = new Text("Home Page");
-        NavBar nav = new NavBar();
-        HBox box = nav.getNavBar();
+        BorderPane parentPanel = new BorderPane();
 
-        // center the vBox and add some padding
-        box.setAlignment(Pos.CENTER);
-        box.setPadding(new Insets(Dashboard.PANEL_PADING));
+        HBox topTitle = new HBox(); //nested panel
+        topTitle.setAlignment(Pos.CENTER);
+        parentPanel.setTop(topTitle);
+        topTitle.setPadding(new Insets(Dashboard.PANEL_PADING));
 
-        box.getChildren().addAll(text); // add as many controls as needed
+        title.setStyle("-fx-font-size: 25px;" +
+                        "-fx-font-family: 'Arial Black'");
+        topTitle.getChildren().addAll(title);
 
-        return new Scene(box, Dashboard.WIN_WIDTH, Dashboard.WIN_HEIGHT);
+        for (int i = 0; i < buttons.length; i++)
+        {
+            Button button = createButton(buttons[i]);
+
+            //parentPanel.setPadding(new Insets(10));
+            parentPanel.setCenter(button);
+        }
+
+        return new Scene(parentPanel, Dashboard.WIN_WIDTH, Dashboard.WIN_HEIGHT);
+    }
+
+    public static Button createButton(String buttonName)
+    {
+        Button button = new Button();
+
+        button.setText(buttonName);
+        button.setPrefHeight(BUTTON_HEIGHT);
+        button.setPrefWidth(BUTTON_WIDTH);
+
+        button.setStyle("-fx-background-color: #81BEF7;" +
+                "-fx-font-family: 'Lucida Console';" +
+                "-fx-font-size: 20px;");
+
+        //button.setPadding(new Insets(10));
+
+        button.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+                Dashboard.switchScenes(buttonName);
+            }
+        });
+
+
+        return button;
     }
 
 }
--- HikingApp/src/main/java/ui/NavBar.java
+++ HikingApp/src/main/java/ui/NavBar.java
@@ -7,6 +7,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -17,7 +19,9 @@ public class NavBar
 
     public HBox getNavBar()
     {
-        HBox box = new HBox();
+        BorderPane parentPanel = new BorderPane();
+
+        HBox nav = new HBox();
 
         //add buttons
         for (int i = 0; i < buttons.length; i++) {
@@ -34,17 +38,19 @@ public class NavBar
                 }
             });
 
-            box.getChildren().add(button);
+            nav.getChildren().add(button);
         }
+
+        parentPanel.setTop(nav);
 
 
         // center the vBox and add some padding
-        box.setAlignment(Pos.CENTER);
-        box.setPadding(new Insets(Dashboard.PANEL_PADING));
+        nav.setAlignment(Pos.CENTER);
+        nav.setPadding(new Insets(Dashboard.PANEL_PADING));
 
          // add as many controls as needed
 
-        return box;
+        return nav;
     }
 
 }
